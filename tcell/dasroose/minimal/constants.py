b0390a840d9ef2e4c08919f69f879ecfedc44c1f
Karmel Allison <karmel@arcaio.com>
2014-08-13 13:55:12 -0700

    Initial rate equation setup

---
+++ tcell/dasroose/minimal/constants.py
@@ -0,0 +1,54 @@
+'''
+Created on Aug 13, 2014
+
+@author: karmel
+'''
+
+class Rates(object):
+    k1f = 0
+    k1r = 0 
+    
+    k2f = 0
+    k2r = 0
+    
+    kcat3 = 0
+    k3f = 0
+    k3r = 0
+    
+    kcat4 = 0
+    k4f = 0
+    k4r = 0
+    
+    kcat5 = 0
+    k5f = 0
+    k5r = 0
+    
+    def __init__(self, *args, **kwargs):
+        '''
+        Allow for keyword-specified rate setting OR just in-order lists.
+        '''
+        super().__init__()
+        
+        if kwargs:
+            for key, val in kwargs.items():
+                self.__setattr__(key, val)
+        else:
+            props = ('k1f', 'k1r', 'k2f', 'k2r',
+                     'kcat3', 'k3f', 'k3r',
+                     'kcat4', 'k4f', 'k4r',
+                     'kcat5', 'k5f', 'k5r',)
+            for i, prop in enumerate(props):
+                try: 
+                    self.__setattr__(prop, args[i])
+                except IndexError: break
+            
+        
+    # Michaelis constants
+    @property
+    def K3m(self): return (self.kcat3 + self.k3r)/self.k3f
+    
+    @property
+    def K4m(self): return (self.kcat4 + self.k4r)/self.k4f
+    
+    @property
+    def K5m(self): return (self.kcat5 + self.k5r)/self.k5f
---
+++ tcell/dasroose/minimal/equations.py
@@ -0,0 +1,31 @@
+'''
+Created on Aug 13, 2014
+
+@author: karmel
+'''
+from tcell.dasroose.minimal.constants import Rates
+
+class Reactions(object):
+    '''
+    Mean-field equations described in Experimental Procedures.
+    Parameters are the concentrations of the specified species. 
+    '''
+    
+    def dSos_dt(self, rates, Sos, Ras_GDP, Ras_GTP, Sos_GDP, Sos_GTP):
+        val = -rates.k1f*Sos*Ras_GDP \
+                + rates.k1r*Sos_GDP \
+                - rates.k2f*Sos*Ras_GTP \
+                + rates.k2r*Sos_GTP
+        return val
+    
+    def dSos_GTP_dt(self, rates, Sos, Ras_GTP, Sos_GTP):
+        val = rates.k2f*Sos*Ras_GTP - rates.k2r*Sos_GTP
+        return val
+        
+    def dRas_GTP_dt(self, rates, Sos, Ras_GDP, Ras_GTP, Sos_GDP, Sos_GTP, Ras_Gap):
+        val = -rates.k2f*Sos*Ras_GTP \
+                + rates.k2r*Sos_GTP \
+                + (rates.kcat3*Ras_GDP*Sos_GTP)/(rates.K3m + Ras_GDP) \
+                + (rates.kcat4*Ras_GDP*Sos_GDP)/(rates.K4m + Ras_GDP) \
+                - (rates.kcat5*Ras_Gap*Ras_GTP)/(rates.K5m + Ras_GTP)
+        return val
